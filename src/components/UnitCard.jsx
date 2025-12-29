559cb33126c4113cf548e0140a4a29946ead1016
mattbayes789 <mattbayes789@gmail.com>
2025-12-29 12:55:43 -0600

    Update UnitCard.jsx

--- src/components/UnitCard.jsx
+++ src/components/UnitCard.jsx
@@ -1,36 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { useDraggable } from "@dnd-kit/core";
+import React from "react";
 
 const UnitCard = ({ unit }) => {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: unit.id
-  });
-
-  const [timeAssigned, setTimeAssigned] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (unit.assignedAt) {
-        setTimeAssigned(Math.floor((Date.now() - unit.assignedAt) / 1000));
-      } else setTimeAssigned(0);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [unit.assignedAt]);
-
   return (
     <div
-      ref={setNodeRef}
-      {...listeners}
-      {...attributes}
-      className="unit-card"
       style={{
-        transform: transform
-          ? `translate3d(${transform.x}px,${transform.y}px,0)`
-          : undefined
+        border: "1px solid #333",
+        padding: "0.5rem",
+        margin: "0.5rem 0",
+        background: "#eee",
+        cursor: "grab"
       }}
     >
-      <strong>{unit.name}</strong>
-      {unit.assignedAt && <div>Time: {timeAssigned}s</div>}
+      {unit.name}
     </div>
   );
 };
