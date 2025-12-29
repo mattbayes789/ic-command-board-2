5128925729043f4a4de807d42425f4b70aba9858
mattbayes789 <mattbayes789@gmail.com>
2025-12-29 11:49:41 -0600

    Create initialUnits.js

---
+++ src/data/initialUnits.js
@@ -0,0 +1,6 @@
+export const initialUnits = [
+  { id: "ENG25", name: "ENG25", assignment: "pool", division: null, assignedAt: null },
+  { id: "TRK22", name: "TRK22", assignment: "pool", division: null, assignedAt: null },
+  { id: "MED1", name: "MED1", assignment: "pool", division: null, assignedAt: null },
+  { id: "BC1", name: "BC1", assignment: "pool", division: null, assignedAt: null }
+];
