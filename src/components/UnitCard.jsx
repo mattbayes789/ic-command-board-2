2dadbabc318a35ae4d6a26cb91f5d19a57643e81
mattbayes789 <mattbayes789@gmail.com>
2025-12-29 13:04:32 -0600

    Update UnitCard.jsx

--- src/components/UnitCard.jsx
+++ src/components/UnitCard.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { useDraggable } from "@dnd-kit/core";
 
 const UnitCard = ({ unit }) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: unit.id
+  });
+
+  const style = {
+    border: "1px solid #333",
+    padding: "0.5rem",
+    margin: "0.5rem 0",
+    background: "#eee",
+    cursor: "grab",
+    transform: transform
+      ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+      : undefined
+  };
+
   return (
-    <div
-      style={{
-        border: "1px solid #333",
-        padding: "0.5rem",
-        margin: "0.5rem 0",
-        background: "#eee",
-        cursor: "grab"
-      }}
-    >
-      {unit.name}
+    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {unit.name} ({unit.timer}s)
     </div>
   );
 };
